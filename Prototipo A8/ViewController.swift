fb3c76958c88e7f0b85e402b1f87624d1c42d3d0
AlumnoUTM <alumnoutm@iMac-de-TECMILENIOVHR-26.local>
2020-03-04 18:21:12 -0600

    Initial Commit

---
+++ Prototipo A8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo A8
+//
+//  Created by AlumnoUTM on 3/4/20.
+//  Copyright © 2020 AlumnoUTM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
